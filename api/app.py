e0e81ba4c450a61c920c809b47c9f56511360dd5
Taylor Blanton <matthewtblanton@gmail.com>
2024-07-08 15:29:14 -0500

    include the infinigram endpoint

--- api/app.py
+++ api/app.py
@@ -4,6 +4,7 @@ import os
 from fastapi import FastAPI
 from src import glog
 from src.health import health_router
+from src.infinigram import infinigram_router
 
 # If LOG_FORMAT is "google:json" emit log message as JSON in a format Google Cloud can parse.
 fmt = os.getenv("LOG_FORMAT")
@@ -13,4 +14,4 @@ logging.basicConfig(level=level, handlers=handlers)
 
 app = FastAPI()
 app.include_router(health_router)
-# app.include_router(router=infinigram_router)
+app.include_router(router=infinigram_router)

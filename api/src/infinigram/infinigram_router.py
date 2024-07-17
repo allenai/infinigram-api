48e8ffa711617171770df02197b8381b04529587
Taylor Blanton <matthewtblanton@gmail.com>
2024-07-17 17:27:29 -0500

    first crack at the attribution endpoint

--- api/src/infinigram/infinigram_router.py
+++ api/src/infinigram/infinigram_router.py
@@ -1,9 +1,11 @@
-from typing import Annotated
+from typing import Annotated, List
 
 from fastapi import APIRouter, Body
 
 from src.infinigram.index_mappings import AvailableInfiniGramIndexId
 from src.infinigram.processor import (
+    InfiniGramAttributionResponse,
+    InfiniGramAttributionResponseWithDocs,
     InfiniGramCountResponse,
     InfiniGramDocumentsResponse,
     InfiniGramProcessorFactoryBodyParamDependency,
@@ -59,5 +61,25 @@ def get_documents(
     search: str,
 ) -> InfiniGramDocumentsResponse:
     result = infini_gram_processor.get_documents(search)
+
+    return result
+
+
+@infinigram_router.post(path="/attribution")
+def get_document_attributions(
+    infini_gram_processor: InfiniGramProcessorFactoryBodyParamDependency,
+    query: Annotated[str, Body(examples=["This is a chat bot response"])],
+    delimiters: Annotated[List[str], Body(examples=[["\n", "."]], default=[])],
+    minimum_span_length: Annotated[int, Body(gt=0, default=5)],
+    maximum_frequency: Annotated[int, Body(gt=0, default=10)],
+    include_documents: Annotated[bool, Body(default=False)],
+) -> InfiniGramAttributionResponse | InfiniGramAttributionResponseWithDocs:
+    result = infini_gram_processor.get_attribution_for_response(
+        search=query,
+        delimiters=delimiters,
+        minimum_span_length=minimum_span_length,
+        maximum_frequency=maximum_frequency,
+        include_documents=include_documents,
+    )
 
     return result

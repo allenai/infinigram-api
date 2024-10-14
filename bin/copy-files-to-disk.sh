a9a508613d875f9241ba2128c39c9bc985441731
Taylor Blanton <matthewtblanton@gmail.com>
2024-10-14 15:24:31 -0500

    add some index additing automation

---
+++ bin/copy-files-to-disk.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+INDEX_NAME=$1
+INDEX_SIZE=$2
+INDEX_BUCKET_NAME=${3:-$INDEX_NAME}
+
+if [[ -z $INDEX_NAME ]]; then 
+    echo "INDEX_NAME is required"
+    exit 1
+fi
+
+if [[ -z $INDEX_SIZE ]]; then 
+    echo "INDEX_SIZE is required"
+    exit 1
+fi
+
+DISK_NAME=infinigram-$INDEX_NAME
+
+echo "Creating disk $DISK_NAME"
+gcloud compute disks create $DISK_NAME \
+    --project=ai2-reviz \
+    --type=pd-balanced \
+    --size=$INDEX_SIZE \
+    --labels=project=infini-gram \
+    --zone=us-west1-b
+
+echo "Creating writer job for $INDEX_NAME"
+# we use envsubst to replace variables in the kubectl yaml template
+INDEX_NAME=$INDEX_NAME \
+ INDEX_SIZE=$INDEX_SIZE \
+ INDEX_BUCKET_NAME=$INDEX_BUCKET_NAME \
+ DISK_NAME=$DISK_NAME \
+ envsubst '$INDEX_NAME:$INDEX_SIZE:$INDEX_BUCKET_NAME:$DISK_NAME' < ./volume-claims/writer-pod.yaml.template | kubectl apply --namespace=infinigram-api -f -
+

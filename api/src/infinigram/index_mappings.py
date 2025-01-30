4df13f9b0f60fece5bf1438ca178ec8b01898c6d
Taylor Blanton <matthewtblanton@gmail.com>
2025-01-30 16:02:15 -0600

    remove dolma as an available index

--- api/src/infinigram/index_mappings.py
+++ api/src/infinigram/index_mappings.py
@@ -9,7 +9,6 @@ from .tokenizers.tokenizer_factory import get_llama_2_tokenizer
 
 class AvailableInfiniGramIndexId(Enum):
     PILEVAL_LLAMA = "pileval-llama"
-    DOLMA_1_7 = "dolma-1_7"
     OLMOE_MIX_0924 = "olmoe-mix-0924"
     OLMOE = "olmoe"
     OLMO_2_1124_13B = "olmo-2-1124-13b"

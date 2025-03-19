98d91876f86fe2aa7dbeb2db2a86ffb8adad508b
Taylor Blanton <matthewtblanton@gmail.com>
2025-03-19 15:40:42 -0500

    fix vendor paths and worker dockerfile

--- attribution-worker/src/tokenizers/tokenizer_factory.py
+++ attribution-worker/src/tokenizers/tokenizer_factory.py
@@ -6,7 +6,7 @@ from .tokenizer import Tokenizer
 @lru_cache
 def get_llama_2_tokenizer() -> Tokenizer:
     return Tokenizer(
-        pretrained_model_name_or_path="./vendor/llama-2-7b-hf",
+        pretrained_model_name_or_path="../vendor/llama-2-7b-hf",
         delimiter_mapping={"\n": 13, ".": 29889},
-        bow_ids_path="./vendor/llama-2_bow_ids.txt",
+        bow_ids_path="../vendor/llama-2_bow_ids.txt",
     )

03e93e88b32094de10b5f3385aa4ecc1c50089d1
IanM <ianmag@cs.washington.edu>
2025-11-04 22:02:35 -0800

    another bad path

--- indexing/dolma2/make_raw_s5cmd_file.py
+++ indexing/dolma2/make_raw_s5cmd_file.py
@@ -28,6 +28,14 @@ with open(f's5cmd_files/raw.s5cmd', 'w') as f:
             else:
                 raise ValueError(f"Cannot extract shard number from {raw_s3_path}")
             raw_s3_path = f's3://ai2-llm/pretraining-data/sources/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve_norefs_mdtables_v2_denylisted/software_dev/step_final/step_final/s2pdf_datadelve_software_dev-{shard_num}.jsonl.gz'
+        elif '/mnt/raid0/pdfs-reshard/software/' in raw_s3_path:
+            import re
+            match = re.search(r'/shard_0*([0-9]+)\.jsonl\.zst$', raw_s3_path)
+            if match:
+                shard_num = match.group(1).zfill(4) if len(match.group(1)) < 4 else match.group(1)
+            else:
+                raise ValueError(f"Cannot extract shard number from {raw_s3_path}")
+            raw_s3_path = f's3://ai2-llm/pretraining-data/sources/s2pdf_dedupe_minhash_v1_with_no_pii_basic_quality_datadelve_norefs_mdtables_v2_denylisted/software/step_final/step_final/s2pdf_datadelve_software-{shard_num}.jsonl.gz'
         else:
             raise NotImplementedError(f'{raw_s3_path} is not supported')
         assert raw_s3_path.startswith('s3://'), raw_s3_path

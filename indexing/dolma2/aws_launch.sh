54cd479bdf39a58bcf397e4615d6b0a0cc220dab
IanM <ianmag@cs.washington.edu>
2025-11-10 17:35:56 -0800

    oops

--- indexing/dolma2/aws_launch.sh
+++ indexing/dolma2/aws_launch.sh
@@ -1,6 +1,6 @@
 # clone the [olmo-cookbook](https://github.com/allenai/olmo-cookbook) repo, and in the [pmr cli](https://github.com/allenai/olmo-cookbook/blob/main/src/cookbook/cli/pmr.py), change user to ubuntu
 
-export AWS_DEFAULT_REGION=us-east-2
+export AWS_DEFAULT_REGION=us-east-1
 export NAME2="infini-gram_dolma2-v02"
 poormanray create --name ${NAME2} --number 24 --instance-type i7i.48xlarge --detach --ami-id ami-084568db4383264d4 --storage-size 32 # ubuntu
 sleep 60

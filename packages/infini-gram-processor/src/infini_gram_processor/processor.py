70ab05cde670a128aff8c4d87de9a2ba3a0accff
Taylor Blanton <matthewtblanton@gmail.com>
2025-11-13 17:29:19 -0600

    default to token_dtype u32. idk if this is right, but it gets further for olmo3 index

--- packages/infini-gram-processor/src/infini_gram_processor/processor.py
+++ packages/infini-gram-processor/src/infini_gram_processor/processor.py
@@ -60,6 +60,7 @@ class InfiniGramProcessor:
             sa_prefetch_depth=0,
             od_prefetch_depth=0,
             vocab_size=self.tokenizer.hf_tokenizer.vocab_size,
+            token_dtype="u32",
         )
 
     @tracer.start_as_current_span("infini_gram_processor/tokenize")

762b0dd353363aab717b76b3da34d90f15435ced
Taylor Blanton <matthewtblanton@gmail.com>
2025-11-14 16:04:22 -0600

    put other indexes back

--- packages/infini-gram-processor/src/infini_gram_processor/index_mappings.py
+++ packages/infini-gram-processor/src/infini_gram_processor/index_mappings.py
@@ -3,17 +3,17 @@ from typing import Iterable, TypedDict
 
 from .processor_config import tokenizer_config
 from .tokenizers.tokenizer import Tokenizer
-from .tokenizers.tokenizer_factory import get_dolma_2_tokenizer
+from .tokenizers.tokenizer_factory import get_dolma_2_tokenizer, get_llama_2_tokenizer
 
 
 class AvailableInfiniGramIndexId(Enum):
-    # PILEVAL_LLAMA = "pileval-llama"
-    # OLMOE_0125_1B_7B = "olmoe-0125-1b-7b"
-    # OLMO_2_1124_13B = "olmo-2-1124-13b"
-    # OLMO_2_0325_32B = "olmo-2-0325-32b"
-    # TULU_3_8B = "tulu-3-8b"
-    # TULU_3_70B = "tulu-3-70b"
-    # TULU_3_405B = "tulu-3-405b"
+    PILEVAL_LLAMA = "pileval-llama"
+    OLMOE_0125_1B_7B = "olmoe-0125-1b-7b"
+    OLMO_2_1124_13B = "olmo-2-1124-13b"
+    OLMO_2_0325_32B = "olmo-2-0325-32b"
+    TULU_3_8B = "tulu-3-8b"
+    TULU_3_70B = "tulu-3-70b"
+    TULU_3_405B = "tulu-3-405b"
     OLMO_3_0625_7B_THINK = "olmo-3-0625-7b-think"
     # OLMO_3_0625_7B_INSTRUCT = "olmo-3-0625-7b-instruct"
     # OLMO_3_0625_32B_THINK = "olmo-3-0625-32b-think"
@@ -30,13 +30,13 @@ class IndexMapping(TypedDict):
 IndexMappings = TypedDict(
     "IndexMappings",
     {
-        # "pileval-llama": IndexMapping,
-        # "olmoe-0125-1b-7b": IndexMapping,
-        # "olmo-2-1124-13b": IndexMapping,
-        # "olmo-2-0325-32b": IndexMapping,
-        # "tulu-3-8b": IndexMapping,
-        # "tulu-3-70b": IndexMapping,
-        # "tulu-3-405b": IndexMapping,
+        "pileval-llama": IndexMapping,
+        "olmoe-0125-1b-7b": IndexMapping,
+        "olmo-2-1124-13b": IndexMapping,
+        "olmo-2-0325-32b": IndexMapping,
+        "tulu-3-8b": IndexMapping,
+        "tulu-3-70b": IndexMapping,
+        "tulu-3-405b": IndexMapping,
         "olmo-3-0625-7b-think": IndexMapping,
         # "olmo-3-0625-7b-instruct": IndexMapping,
         # "olmo-3-0625-32b-think": IndexMapping,
@@ -44,59 +44,59 @@ IndexMappings = TypedDict(
 )
 
 index_mappings: IndexMappings = {
-    # AvailableInfiniGramIndexId.PILEVAL_LLAMA.value: {
-    #     "tokenizer": get_llama_2_tokenizer(),
-    #     "index_dir": f"{tokenizer_config.index_base_path}/v4_pileval_llama",
-    #     "index_dir_diff": [],
-    # },
-    # AvailableInfiniGramIndexId.OLMOE_0125_1B_7B.value: {
-    #     "tokenizer": get_llama_2_tokenizer(),
-    #     "index_dir": [
-    #         f"{tokenizer_config.index_base_path}/olmoe-mix-0924-dclm",
-    #         f"{tokenizer_config.index_base_path}/olmoe-mix-0924-nodclm",
-    #         f"{tokenizer_config.index_base_path}/v4-olmoe-0125-1b-7b-anneal-adapt",
-    #     ],
-    #     "index_dir_diff": [],
-    # },
-    # AvailableInfiniGramIndexId.OLMO_2_1124_13B.value: {
-    #     "tokenizer": get_llama_2_tokenizer(),
-    #     "index_dir": [
-    #         f"{tokenizer_config.index_base_path}/olmoe-mix-0924-dclm",
-    #         f"{tokenizer_config.index_base_path}/olmoe-mix-0924-nodclm",
-    #         f"{tokenizer_config.index_base_path}/v4-olmo-2-1124-13b-anneal-adapt",
-    #     ],
-    #     "index_dir_diff": [],
-    # },
-    # AvailableInfiniGramIndexId.OLMO_2_0325_32B.value: {
-    #     "tokenizer": get_llama_2_tokenizer(),
-    #     "index_dir": [
-    #         f"{tokenizer_config.index_base_path}/olmoe-mix-0924-dclm",
-    #         f"{tokenizer_config.index_base_path}/olmoe-mix-0924-nodclm",
-    #         f"{tokenizer_config.index_base_path}/v4-olmo-2-0325-32b-anneal-adapt",
-    #     ],
-    #     "index_dir_diff": [],
-    # },
-    # AvailableInfiniGramIndexId.TULU_3_8B.value: {
-    #     "tokenizer": get_llama_2_tokenizer(),
-    #     "index_dir": [
-    #         f"{tokenizer_config.index_base_path}/v4-tulu-3-8b-adapt",
-    #     ],
-    #     "index_dir_diff": [],
-    # },
-    # AvailableInfiniGramIndexId.TULU_3_70B.value: {
-    #     "tokenizer": get_llama_2_tokenizer(),
-    #     "index_dir": [
-    #         f"{tokenizer_config.index_base_path}/v4-tulu-3-70b-adapt",
-    #     ],
-    #     "index_dir_diff": [],
-    # },
-    # AvailableInfiniGramIndexId.TULU_3_405B.value: {
-    #     "tokenizer": get_llama_2_tokenizer(),
-    #     "index_dir": [
-    #         f"{tokenizer_config.index_base_path}/v4-tulu-3-405b-adapt",
-    #     ],
-    #     "index_dir_diff": [],
-    # },
+    AvailableInfiniGramIndexId.PILEVAL_LLAMA.value: {
+        "tokenizer": get_llama_2_tokenizer(),
+        "index_dir": f"{tokenizer_config.index_base_path}/v4_pileval_llama",
+        "index_dir_diff": [],
+    },
+    AvailableInfiniGramIndexId.OLMOE_0125_1B_7B.value: {
+        "tokenizer": get_llama_2_tokenizer(),
+        "index_dir": [
+            f"{tokenizer_config.index_base_path}/olmoe-mix-0924-dclm",
+            f"{tokenizer_config.index_base_path}/olmoe-mix-0924-nodclm",
+            f"{tokenizer_config.index_base_path}/v4-olmoe-0125-1b-7b-anneal-adapt",
+        ],
+        "index_dir_diff": [],
+    },
+    AvailableInfiniGramIndexId.OLMO_2_1124_13B.value: {
+        "tokenizer": get_llama_2_tokenizer(),
+        "index_dir": [
+            f"{tokenizer_config.index_base_path}/olmoe-mix-0924-dclm",
+            f"{tokenizer_config.index_base_path}/olmoe-mix-0924-nodclm",
+            f"{tokenizer_config.index_base_path}/v4-olmo-2-1124-13b-anneal-adapt",
+        ],
+        "index_dir_diff": [],
+    },
+    AvailableInfiniGramIndexId.OLMO_2_0325_32B.value: {
+        "tokenizer": get_llama_2_tokenizer(),
+        "index_dir": [
+            f"{tokenizer_config.index_base_path}/olmoe-mix-0924-dclm",
+            f"{tokenizer_config.index_base_path}/olmoe-mix-0924-nodclm",
+            f"{tokenizer_config.index_base_path}/v4-olmo-2-0325-32b-anneal-adapt",
+        ],
+        "index_dir_diff": [],
+    },
+    AvailableInfiniGramIndexId.TULU_3_8B.value: {
+        "tokenizer": get_llama_2_tokenizer(),
+        "index_dir": [
+            f"{tokenizer_config.index_base_path}/v4-tulu-3-8b-adapt",
+        ],
+        "index_dir_diff": [],
+    },
+    AvailableInfiniGramIndexId.TULU_3_70B.value: {
+        "tokenizer": get_llama_2_tokenizer(),
+        "index_dir": [
+            f"{tokenizer_config.index_base_path}/v4-tulu-3-70b-adapt",
+        ],
+        "index_dir_diff": [],
+    },
+    AvailableInfiniGramIndexId.TULU_3_405B.value: {
+        "tokenizer": get_llama_2_tokenizer(),
+        "index_dir": [
+            f"{tokenizer_config.index_base_path}/v4-tulu-3-405b-adapt",
+        ],
+        "index_dir_diff": [],
+    },
     AvailableInfiniGramIndexId.OLMO_3_0625_7B_THINK.value: {
         "tokenizer": get_dolma_2_tokenizer(),
         "index_dir": [

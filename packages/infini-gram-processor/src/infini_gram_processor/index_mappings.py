378f3fce574fcc3b80e6e64b4e2d5ec90ca0400f
Taylor Blanton <matthewtblanton@gmail.com>
2025-11-14 12:20:58 -0600

    remove olmo3 indexes to see if that helps anything

--- packages/infini-gram-processor/src/infini_gram_processor/index_mappings.py
+++ packages/infini-gram-processor/src/infini_gram_processor/index_mappings.py
@@ -3,7 +3,7 @@ from typing import Iterable, TypedDict
 
 from .processor_config import tokenizer_config
 from .tokenizers.tokenizer import Tokenizer
-from .tokenizers.tokenizer_factory import get_dolma_2_tokenizer, get_llama_2_tokenizer
+from .tokenizers.tokenizer_factory import get_llama_2_tokenizer
 
 
 class AvailableInfiniGramIndexId(Enum):
@@ -14,9 +14,9 @@ class AvailableInfiniGramIndexId(Enum):
     TULU_3_8B = "tulu-3-8b"
     TULU_3_70B = "tulu-3-70b"
     TULU_3_405B = "tulu-3-405b"
-    OLMO_3_0625_7B_THINK = "olmo-3-0625-7b-think"
-    OLMO_3_0625_7B_INSTRUCT = "olmo-3-0625-7b-instruct"
-    OLMO_3_0625_32B_THINK = "olmo-3-0625-32b-think"
+    # OLMO_3_0625_7B_THINK = "olmo-3-0625-7b-think"
+    # OLMO_3_0625_7B_INSTRUCT = "olmo-3-0625-7b-instruct"
+    # OLMO_3_0625_32B_THINK = "olmo-3-0625-32b-think"
 
 
 class IndexMapping(TypedDict):
@@ -37,9 +37,9 @@ IndexMappings = TypedDict(
         "tulu-3-8b": IndexMapping,
         "tulu-3-70b": IndexMapping,
         "tulu-3-405b": IndexMapping,
-        "olmo-3-0625-7b-think": IndexMapping,
-        "olmo-3-0625-7b-instruct": IndexMapping,
-        "olmo-3-0625-32b-think": IndexMapping,
+        # "olmo-3-0625-7b-think": IndexMapping,
+        # "olmo-3-0625-7b-instruct": IndexMapping,
+        # "olmo-3-0625-32b-think": IndexMapping,
     },
 )
 
@@ -97,27 +97,27 @@ index_mappings: IndexMappings = {
         ],
         "index_dir_diff": [],
     },
-    AvailableInfiniGramIndexId.OLMO_3_0625_7B_THINK.value: {
-        "tokenizer": get_dolma_2_tokenizer(),
-        "index_dir": [
-            f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
-            f"{tokenizer_config.index_base_path}/dolma2-0625-v01-7b",
-        ],
-        "index_dir_diff": [],
-    },
-    AvailableInfiniGramIndexId.OLMO_3_0625_7B_INSTRUCT.value: {
-        "tokenizer": get_dolma_2_tokenizer(),
-        "index_dir": [
-            f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
-            f"{tokenizer_config.index_base_path}/dolma2-0625-v01-7b",
-        ],
-        "index_dir_diff": [],
-    },
-    AvailableInfiniGramIndexId.OLMO_3_0625_32B_THINK.value: {
-        "tokenizer": get_dolma_2_tokenizer(),
-        "index_dir": [
-            f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
-        ],
-        "index_dir_diff": [],
-    },
+    # AvailableInfiniGramIndexId.OLMO_3_0625_7B_THINK.value: {
+    #     "tokenizer": get_dolma_2_tokenizer(),
+    #     "index_dir": [
+    #         f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
+    #         f"{tokenizer_config.index_base_path}/dolma2-0625-v01-7b",
+    #     ],
+    #     "index_dir_diff": [],
+    # },
+    # AvailableInfiniGramIndexId.OLMO_3_0625_7B_INSTRUCT.value: {
+    #     "tokenizer": get_dolma_2_tokenizer(),
+    #     "index_dir": [
+    #         f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
+    #         f"{tokenizer_config.index_base_path}/dolma2-0625-v01-7b",
+    #     ],
+    #     "index_dir_diff": [],
+    # },
+    # AvailableInfiniGramIndexId.OLMO_3_0625_32B_THINK.value: {
+    #     "tokenizer": get_dolma_2_tokenizer(),
+    #     "index_dir": [
+    #         f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
+    #     ],
+    #     "index_dir_diff": [],
+    # },
 }

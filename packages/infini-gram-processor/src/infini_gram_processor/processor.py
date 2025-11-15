84c29109b6a6ffc7e04216d10d560c601d8778e2
Taylor Blanton <matthewtblanton@gmail.com>
2025-11-14 18:29:09 -0600

    Revert "put token_dtype back, track detail in an attribute"
    This reverts commit ce0e039303d9163082661ec0cb1271b9abf403d6.

--- packages/infini-gram-processor/src/infini_gram_processor/processor.py
+++ packages/infini-gram-processor/src/infini_gram_processor/processor.py
@@ -61,7 +61,7 @@ class InfiniGramProcessor:
             sa_prefetch_depth=0,
             od_prefetch_depth=0,
             vocab_size=self.tokenizer.hf_tokenizer.vocab_size,
-            token_dtype="u32",
+            token_dtype=index_mapping["token_dtype"],
         )
 
     @tracer.start_as_current_span("infini_gram_processor/tokenize")
@@ -86,9 +86,7 @@ class InfiniGramProcessor:
             exception = InfiniGramEngineException(detail=result["error"])
 
             current_span = trace.get_current_span()
-            current_span.record_exception(
-                exception, attributes={"detail": exception.detail}
-            )
+            current_span.record_exception(exception)
             current_span.set_status(StatusCode.ERROR)
 
             raise exception

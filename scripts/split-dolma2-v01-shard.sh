e1fe6c6d249ce7cb0ba9c72726e58d8ac9ed6661
Taylor Blanton <matthewtblanton@gmail.com>
2025-11-17 12:29:14 -0600

    try bringing number of volume mounts down

--- scripts/split-dolma2-v01-shard.sh
+++ scripts/split-dolma2-v01-shard.sh
@@ -4,6 +4,6 @@ declare -a shards=("00" "01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "
 
 for shard in "${shards[@]}"
 do
-   gcloud storage cp -r "gs://infinigram/index/dolma2-0625-v01/${shard}/" "gs://infinigram/index/v6-dolma2-0625-v01-7b/${shard}"
+   gcloud storage cp -r "gs://infinigram/index/dolma2-0625-v01/${shard}/" "gs://infinigram/index/v6-dolma2-0625-v01-base-shared/${shard}"
     
 done

2e8f145743b6f4b39c6cfd05064c6ae744446058
Taylor Blanton <matthewtblanton@gmail.com>
2025-11-17 14:36:36 -0600

    back to just one index

--- packages/infini-gram-processor/src/infini_gram_processor/index_mappings.py
+++ packages/infini-gram-processor/src/infini_gram_processor/index_mappings.py
@@ -8,8 +8,8 @@ from .tokenizers.tokenizer_factory import get_dolma_2_tokenizer
 
 class AvailableInfiniGramIndexId(Enum):
     OLMO_3_0625_7B_THINK = "olmo-3-0625-7b-think"
-    OLMO_3_0625_7B_INSTRUCT = "olmo-3-0625-7b-instruct"
-    OLMO_3_0625_32B_THINK = "olmo-3-0625-32b-think"
+    # OLMO_3_0625_7B_INSTRUCT = "olmo-3-0625-7b-instruct"
+    # OLMO_3_0625_32B_THINK = "olmo-3-0625-32b-think"
 
 
 class IndexMapping(TypedDict):
@@ -25,8 +25,8 @@ IndexMappings = TypedDict(
     "IndexMappings",
     {
         "olmo-3-0625-7b-think": IndexMapping,
-        "olmo-3-0625-7b-instruct": IndexMapping,
-        "olmo-3-0625-32b-think": IndexMapping,
+        # "olmo-3-0625-7b-instruct": IndexMapping,
+        # "olmo-3-0625-32b-think": IndexMapping,
     },
 )
 
@@ -40,21 +40,21 @@ index_mappings: IndexMappings = {
         "index_dir_diff": [],
         "token_dtype": "u32",
     },
-    AvailableInfiniGramIndexId.OLMO_3_0625_7B_INSTRUCT.value: {
-        "tokenizer": get_dolma_2_tokenizer(),
-        "index_dir": [
-            f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
-            f"{tokenizer_config.index_base_path}/dolma2-0625-v01-7b",
-        ],
-        "index_dir_diff": [],
-        "token_dtype": "u32",
-    },
-    AvailableInfiniGramIndexId.OLMO_3_0625_32B_THINK.value: {
-        "tokenizer": get_dolma_2_tokenizer(),
-        "index_dir": [
-            f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
-        ],
-        "index_dir_diff": [],
-        "token_dtype": "u32",
-    },
+    # AvailableInfiniGramIndexId.OLMO_3_0625_7B_INSTRUCT.value: {
+    #     "tokenizer": get_dolma_2_tokenizer(),
+    #     "index_dir": [
+    #         f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
+    #         f"{tokenizer_config.index_base_path}/dolma2-0625-v01-7b",
+    #     ],
+    #     "index_dir_diff": [],
+    #     "token_dtype": "u32",
+    # },
+    # AvailableInfiniGramIndexId.OLMO_3_0625_32B_THINK.value: {
+    #     "tokenizer": get_dolma_2_tokenizer(),
+    #     "index_dir": [
+    #         f"{tokenizer_config.index_base_path}/dolma2-0625-base-shared",
+    #     ],
+    #     "index_dir_diff": [],
+    #     "token_dtype": "u32",
+    # },
 }

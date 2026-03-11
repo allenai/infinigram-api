fdaf275be4574d57b87e47e99fe8a6cf2ef29c70
Taylor Blanton <matthewtblanton@gmail.com>
2026-03-11 12:12:22 -0500

    remove the indexes from the available index id enum too

--- packages/infini-gram-processor/src/infini_gram_processor/index_mappings.py
+++ packages/infini-gram-processor/src/infini_gram_processor/index_mappings.py
@@ -14,9 +14,9 @@ class AvailableInfiniGramIndexId(Enum):
     # TULU_3_8B = "tulu-3-8b"
     # TULU_3_70B = "tulu-3-70b"
     # TULU_3_405B = "tulu-3-405b"
-    OLMO_3_0625_7B_THINK = "olmo-3-0625-7b-think"
-    OLMO_3_0625_7B_INSTRUCT = "olmo-3-0625-7b-instruct"
-    OLMO_3_0625_32B_THINK = "olmo-3-0625-32b-think"
+    # OLMO_3_0625_7B_THINK = "olmo-3-0625-7b-think"
+    # OLMO_3_0625_7B_INSTRUCT = "olmo-3-0625-7b-instruct"
+    # OLMO_3_0625_32B_THINK = "olmo-3-0625-32b-think"
     OLMO_3_0625_32B_INSTRUCT = "olmo-3-0625-32b-instruct"
 
 

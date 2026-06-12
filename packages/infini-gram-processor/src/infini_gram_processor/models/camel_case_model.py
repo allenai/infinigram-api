ee54957bccaa4959c1832427647736c1f4316e79
Taylor Blanton <matthewtblanton@gmail.com>
2026-06-12 10:18:06 -0500

    i got too fancy with the mixin, reverting that and just copying config

--- packages/infini-gram-processor/src/infini_gram_processor/models/camel_case_model.py
+++ packages/infini-gram-processor/src/infini_gram_processor/models/camel_case_model.py
@@ -2,13 +2,13 @@ from pydantic import BaseModel
 from pydantic.alias_generators import to_camel
 
 
-class CamelCaseConfigMixin:
+class CamelCaseModel(BaseModel):
     class Config:
         alias_generator = to_camel
         populate_by_name = True
 
 
-class CamelCaseModel(BaseModel, CamelCaseConfigMixin): ...
-
-
-class ApiRequest(BaseModel, CamelCaseConfigMixin, frozen=True): ...
+class ApiRequest(BaseModel, frozen=True):
+    class Config:
+        alias_generator = to_camel
+        populate_by_name = True
